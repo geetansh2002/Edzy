2df43fded24ff9e7efb9af923b8bfcbebe139f9d
SharadPratapSS <sharadsengar2003@gmail.com>
2024-03-29 23:05:28 +0530

    Logins 2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     androidTestImplementation(libs.androidx.espresso.core)
 }
