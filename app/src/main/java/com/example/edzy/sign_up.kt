2f398de2319c15378f4a6af208f6e8d00142ffc9
SharadPratapSS <sharadsengar2003@gmail.com>
2024-03-30 10:36:00 +0530

    login and signup1

--- app/src/main/java/com/example/edzy/sign_up.kt
+++ app/src/main/java/com/example/edzy/sign_up.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class login_page : AppCompatActivity() {
+class sign_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.`signup.xml`)
+        setContentView(R.layout.activity_sign_up)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
