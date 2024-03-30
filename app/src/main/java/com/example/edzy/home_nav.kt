47522392880cc4895093dcfa3100a226c9b64af3
SharadPratapSS <sharadsengar2003@gmail.com>
2024-03-30 14:44:06 +0530

    login and signup1

--- app/src/main/java/com/example/edzy/home_nav.kt
+++ app/src/main/java/com/example/edzy/home_nav.kt
@@ -1,10 +1,12 @@
 package com.example.edzy
 
+import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.edzy.R.layout.fragment_home_nav
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,13 +30,25 @@ class home_nav : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
+//    fun showLogoutDialog() {
+//        val builder = AlertDialog.Builder(this)
+//        builder.setView(R.layout.logout_popup)
+//            .setPositiveButton("Yes") { dialog, which ->
+//
+//            }
+//            .setNegativeButton("No") { dialog, which ->
+//                dialog.dismiss()
+//            }
+//            .show()
+//    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_nav, container, false)
+        return inflater.inflate(fragment_home_nav, container, false)
     }
 
     companion object {
